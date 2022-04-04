9f9dbb1b9a3240bd9d5218ac25a9d06d178fe5c0
M Furkan Yiğit <74006145+mfy98@users.noreply.github.com>
2022-04-04 12:34:39 +0300

    txt username alphabet

--- LoginForm/Form1.cs
+++ LoginForm/Form1.cs
@@ -29,6 +29,23 @@ namespace LoginForm
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+            if (!System.Text.RegularExpressions.Regex.IsMatch(txtUsername.Text, "^[a-zA-Z ]"))
+            {
+                MessageBox.Show("This textbox accepts only alphabetical characters");
+                txtUsername.Text.Remove(txtUsername.Text.Length - 1);
+            }
+
+            List<string> allvalues = this.Controls.OfType<TextBox>().Select(x => x.Text).ToList();
+
+            System.IO.File.WriteAllText("C:\\Users\\Administrator\\Documents\\login.txt", String.Join(Environment.NewLine, allvalues));
+            string[] alllines = System.IO.File.ReadAllLines("C:\\Users\\Administrator\\Documents\\login.txt");
+
+            List<TextBox> allTextboxes = this.Controls.OfType<TextBox>().ToList();
+            for (int i = 0; i < allTextboxes.Count; i++)
+            {
+                allTextboxes[i].Text = alllines[i];
+            }
+            
         }
 
         
